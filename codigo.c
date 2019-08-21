86b0eb91ff804a1e30030d661e3194697d315a4b
Tomas Gueiros Souza Manzi <sanshoo@pop-os.localdomain>
2019-08-20 21:05:37 -0300

    corrigidos bugs, ainda tem bugs

--- codigo.c
+++ codigo.c
@@ -3,15 +3,15 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#define MAX LINE 80 /* The maximum length command */
+#define MAX_LINE 80 /* The maximum length command */
 
 
         
 int main(void)
 {
-char *args[MAX LINE/2 + 1]; /* command line arguments */
-int should run = 1; /* flag to determine when to exit program */
-while (should run) {
+char *args[MAX_LINE/2 + 1]; /* command line arguments */
+int should_run = 1; /* flag to determine when to exit program */
+while (should_run) {
 printf("osh>");
 fflush(stdout);
 }
